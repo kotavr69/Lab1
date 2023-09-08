0d548fecd285c6f2bd59f4378473b0fcc8a3d8de
kotavr69 <144414802+kotavr69@users.noreply.github.com>
2023-09-08 14:48:09 +0700

    lab1
    lab1

---
+++ KM_1.cpp
@@ -0,0 +1,38 @@
+﻿/*Приложение А.Исходный код программы на Си
+#include <locale.h>
+#include <stdio.h>
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    printf("Hello World! Привет мир!\n")
+        int var = 42;
+    int unusing;
+}*/
+
+/*Приложение Б. Исходный код программы на С++
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+    		setlocale(LC_ALL, "ru");
+
+    		cout << "Hello World! Привет мир!" << endl;
+    		int var = 42;
+    		int unusing;
+}
+*/
+
+/*Приложение С. Исправленный код на С++
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+    		cout << "Hello World! Привет мир!" << endl;
+}
+
+*/
